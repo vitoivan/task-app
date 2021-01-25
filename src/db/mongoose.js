4f22c325423e12a17c255e7f3f76ab214172b4f8
Victor Ivan <viictor.ivan@gmail.com>
2021-01-25 16:13:05 -0300

    Validando email com biblioteca validator

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
     useNewUrlParser: true,
@@ -8,20 +9,35 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
 
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true
     },
     age: {
-        type: Number
+        type: Number,
+        required: true,
+        validate(age){
+            if (age < 0) throw new Error("Age must be a positive number")
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        validate(email){
+            if(!validator.isEmail(email )) throw new Error("Invalid email")
+        }
     }
+    
 })
-
+// --------------------- New User -------------------------------- //
 const user1 = new User({
-    name: 'Duda',
-    age: 22
+    name: 'Ivan Victor',
+    age: 85,
+    email: 'viictor.ivan@hotmail.com'
 })
 
+// --------------------- Promise depois de salvar o user na collection -------------------------------- //
 user1.save().then((r) => {
     console.log(user1)
 }).catch((e) =>{
-    console.log(e)
+    console.log('Error:  ' + e)
 })
