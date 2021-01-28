0c35a8adc5232d990550aecd00361cd0e3957fc2
Victor Ivan <viictor.ivan@gmail.com>
2021-01-28 12:38:51 -0300

    Taskapp routes

--- src/index.js
+++ src/index.js
@@ -8,29 +8,63 @@ const app = express()
 const port = process.env.PORT || 3000
 app.use(express.json())
 
+// -------------------------- Add um usuario  ------------------------- //
 app.post("/users", (req, res) =>{
     
     const user = new User(req.body)
-    user.save().then( () => {
+    user.save().then(  () => res.status(201).send(user)  ).catch(  e => res.status(400).send(e)  )
+
+})
+
+
+
+// --------------------- Listar todos os usuarios ----------------------- //
+app.get('/users', (req, res) => {
+    
+    User.find({}).then(  users => res.send(users)  ).catch(  e => res.status(500).send(e)  )
+
+})
+
+
+// ----------------------- Listar apenas um usuario ----------------------- //
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+    User.findById(_id).then(  user => {
+
+        if (!user) return res.status(404).send()
         res.send(user)
-    }).catch( (e) => {
-        res.status(400)
-        res.send(e)
-    })
+
+    }).catch(  e => res.status(500).send(console.log(e))  )
+
 })
 
+
+// ----------------------- Add uma task ------------------------------ //
 app.post("/tasks", (req, res) => {
+
     const task = new Task(req.body)
-    task.save().then( () => {
+    task.save().then(  () => res.status(201).send(task)  ).catch(  e =>  res.status(400).send(e)  )
+
+})
+
+
+// ---------------------- Listar todas as tasks ----------------------------- //
+app.get("/tasks", (req, res) => {
+
+    Task.find({}).then(  tasks => res.send(tasks)  ).catch(  error => res.status(500).send(error)  )
+})
+
+
+// ---------------------- Listar uma task ------------------------------------ //
+app.post('/tasks/:id', (req, res) => {
+    const _id = req.params.id
+    Task.findById(_id).then(  task => {
+        if(!task) return res.status(404).send()
         res.send(task)
-    }).catch( (e) => {
-        res.status(400)
-        res.send(e)
-    })
+
+    }).catch(  e => console.log(e)  )
 })
 
 
-app.listen(port, () => {
-    console.log(`Server is up on port ${port}`)
-})
+app.listen(  port, () => console.log(`Server is up on port ${port}`)  )
 
